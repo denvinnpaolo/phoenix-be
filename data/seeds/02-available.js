bd1fe597a014d2b44be4f6873327eb440913d73d
Denvinn Magsino <denvinnpaolo@gmail.com>
2020-12-18 19:00:36 -0800

    added sed files

--- data/seeds/02-available.js
+++ data/seeds/02-available.js
@@ -9,18 +9,92 @@ exports.seed = function(knex) {
         address: "123 lane st",
         description: "organic waste",
         time_available: "morning",
-        exp: "2020-11-26",
+        exp: "2020-12-26",
         items: "Bag 2 XL, 3 S",
-        date_posted: "2020-11-23"},
-        {id:2,
+        date_posted: "2020-12-23"},
+        {
+          id:2,
           type: "hotel",
           producer_id: 2,
           address: "421 ave st",
           description: "organic waste",
           time_available: "afternoon",
-          exp: "2020-12-12",
+          exp: "2020-12-22",
           items: "Bag 2 XL, 3 S",
-          date_posted: "2020-12-08"},
+          date_posted: "2020-12-08"
+        },
+        {
+          id:3,
+          type: "business",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "evening",
+          exp: "2020-12-29",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-08"
+        },        {
+          id:4,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "afternoon",
+          exp: "2020-12-25",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-19"
+        },        {
+          id:5,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "morning",
+          exp: "2020-12-23",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-18"
+        },        {
+          id:6,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "afternoon",
+          exp: "2020-27-12",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-19"
+        },        {
+          id:7,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "afternoon",
+          exp: "2020-12-24",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-18"
+        },        {
+          id:8,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "morning",
+          exp: "2020-12-22",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-15"
+        },        
+        {
+          id:9,
+          type: "hotel",
+          producer_id: 2,
+          address: "421 ave st",
+          description: "organic waste",
+          time_available: "evening",
+          exp: "2020-12-31",
+          items: "Bag 2 XL, 3 S",
+          date_posted: "2020-12-20"
+        },
 
       ]);
 };
